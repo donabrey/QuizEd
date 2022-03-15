0497667e098b39a7b72d7d0e36569b6aef52a596
Donovan Abrey <donovan@itsolutions.co.za>
2022-03-15 14:57:38 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,37 @@
+﻿//Settings
+var randomizeAnswers = true;
+var resultsHeading = '<h3>Results</h3>';
+
+//Questions
+let questions = [
+    {
+        number: 1,
+        question: 'What is the capital of Australia?',
+        answers: [
+            { answer: 'Sydney', correct: false },
+            { answer: 'Canberra', correct: true },
+            { answer: 'Perth', correct: false },
+            { answer: 'Melbourne', correct: false }
+        ]
+    },
+    {
+        number: 2,
+        question: 'What is the capital of New Zealand?',
+        answers: [
+            { answer: 'Auckland', correct: false },
+            { answer: 'Christchurch', correct: false },
+            { answer: 'Queenstown', correct: false },
+            { answer: 'Wellington', correct: true }
+        ]
+    },
+    {
+        number: 3,
+        question: 'What is the capital of the United States?',
+        answers: [
+            { answer: 'Washington D.C.', correct: true },
+            { answer: 'Seattle', correct: false },
+            { answer: 'New York', correct: false },
+            { answer: ' San Francisco', correct: false }
+        ]
+    }
+]
