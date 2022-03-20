31211f3be23c0dedb3bfff381a0d5267bf33a3a2
Donovan Abrey <donovan@itsolutions.co.za>
2022-03-20 22:55:21 +0200

    Rounding of final percentage of results.

---
+++ quized-bootstrap.js
@@ -0,0 +1,112 @@
+﻿const quiz = document.getElementById('quiz');
+const results = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const answerLabels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+
+previousButton.addEventListener('click', showPreviousQuestion);
+nextButton.addEventListener('click', showNextQuestion);
+submitButton.addEventListener('click', checkUserAnswers);
+
+let questionAnswers = [];
+
+questions.forEach(function (currentQuestion, questionNumber) {
+    var output = '';
+    output += '<div class="question-block"><div class="card"><div class="card-header"><h5 class="card-title">' + currentQuestion.question + '</h5></div>';
+    output += '<div class="answers card-body">';
+    let answerOrder = randomOrder(currentQuestion.answers.length);
+
+    if (randomizeAnswers) {
+        for (let i = 0; i < currentQuestion.answers.length; i++) {
+            output += '<div class="form-check"><input class="form-check-input" type="radio" id="' + questionNumber + '_' + i + '" name="question' + questionNumber + '" value="' + currentQuestion.answers[answerOrder[i]].answer + '"><label class="form-check-label" id="' + questionNumber + '_' + i + '_label" for="' + questionNumber + '_' + i + '">' + answerLabels[i] + ': ' + currentQuestion.answers[answerOrder[i]].answer + '</label></div>';
+            questionAnswer = { questionNumber: currentQuestion.number, questionAnswerNumber: questionNumber + '_' + i, correct: currentQuestion.answers[answerOrder[i]].correct };
+            questionAnswers.push(questionAnswer);
+        }
+    }
+    else {
+        currentQuestion.answers.forEach(function (currentAnswer, answerNumber) {
+            output += '<div class="form-check"><input class="form-check-input" type="radio" id="' + questionNumber + '_' + answerNumber + '" name="question' + questionNumber + '" value="' + currentAnswer.answer + '"><label class="form-check-label" id="' + questionNumber + '_' + answerNumber + '_label" for="' + questionNumber + '_' + answerNumber + '">' + answerLabels[answerNumber] + ': ' + currentAnswer.answer + '</label></div>';
+            questionAnswer = { questionNumber: currentQuestion.number, questionAnswerNumber: questionNumber + '_' + answerNumber, correct: currentAnswer.correct };
+            questionAnswers.push(questionAnswer);
+        });
+    }
+
+    output += "</div></div></div>";
+    quiz.innerHTML += output;
+});
+
+const questionBlocks = document.querySelectorAll('.question-block');
+let currentQuestion = 0;
+function showQuestion(n) {
+    questionBlocks[currentQuestion].classList.remove('active-question');
+    questionBlocks[n].classList.add('active-question');
+
+    currentQuestion = n;
+    if (currentQuestion === 0) {
+        previousButton.style.display = 'none';
+    }
+    else {
+        previousButton.style.display = 'inline-block';
+    }
+    if (currentQuestion === questionBlocks.length - 1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function checkUserAnswers() {
+    results.innerHTML = resultsHeading;
+    var correctAnswers = 0;
+    questionAnswers.forEach(function (currentAnswer, answerNumber) {
+        var answer = document.getElementById(currentAnswer.questionAnswerNumber);
+        var answerLabel = document.getElementById(currentAnswer.questionAnswerNumber + '_label');
+        answerLabel.classList.remove('correct', 'incorrect');
+        if (answer.checked == true) {
+            if (currentAnswer.correct) {
+                results.innerHTML += '<span class="correct">Question ' + currentAnswer.questionNumber + ' was answered correctly.</span><br/>';
+                answerLabel.classList.add('correct');
+                correctAnswers++;
+            }
+            else {
+                results.innerHTML += '<span class="incorrect">Question ' + currentAnswer.questionNumber + ' was not answered correctly.</span><br />';
+                answerLabel.classList.add('incorrect');
+            }
+        }
+
+    });
+    results.innerHTML += '<p>' + correctAnswers + ' out of ' + questions.length + ' (' + Math.round(correctAnswers / questions.length * 100) + '%)</p>';
+}
+
+showQuestion(currentQuestion);
+
+function showNextQuestion() {
+    showQuestion(currentQuestion + 1);
+}
+
+function showPreviousQuestion() {
+    showQuestion(currentQuestion - 1);
+}
+
+function randomOrder(numAnswers) {
+    let answerOrder = [];
+    var count = 0;
+    do {
+        var random = Math.floor((Math.random() * numAnswers));
+        var isRandom = true;
+        for (let i = 0; i < numAnswers; i++) {
+            if (answerOrder[i] == random) {
+                isRandom = false;
+            }
+        }
+        if (isRandom) {
+            answerOrder.push(random);
+            count++;
+        }
+    } while (count < numAnswers);
+    return answerOrder;
+}
---
+++ settings.js
@@ -0,0 +1,7 @@
+﻿/* QuizEd Settings */
+
+//randomize answer choices
+var randomizeAnswers = true;
+
+//heading display above results
+var resultsHeading = '<h4>Results</h4>';
